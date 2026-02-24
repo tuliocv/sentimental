0967bf49184005d1641da859e2461b4bf118cd45
tuliocv <130805106+tuliocv@users.noreply.github.com>
2026-02-24 12:03:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -184,11 +184,6 @@ def admin_panel(df: pd.DataFrame):
 # Mini visual do ET (sem imagem externa)
 # =========================
 ET_MINI = r'''
-<pre style="font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas;
-            line-height: 1.2; margin: 0; font-size: 16px;">
-    👽
-   
-</pre>
 '''
 
 # =========================
